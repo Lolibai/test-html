678b33e8958d50d39cd20e6325631dbda89218d8
Lolibai <mikleshef@gmail.com>
2019-08-02 21:23:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>Ivan Saliuk</code>.
+          Hello <code>Ivan Ivan</code>.
         </p>
         <a
           className="App-link"
